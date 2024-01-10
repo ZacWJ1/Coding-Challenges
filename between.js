8059e7c4227cff4a1cd3bcd3a4c60687368ce35c
ZacWJ1 <142571679+ZacWJ1@users.noreply.github.com>
2024-01-09 22:36:52 -0800

    codewars

---
+++ between.js
@@ -0,0 +1,10 @@
+//what is between
+function between(a, b) {
+    // create an array to push values into
+    let list =[]
+    //create a loop that pushes the values
+    for(let i = a; i <= b; i++){
+      list.push(i)
+    }
+    return list
+  }
